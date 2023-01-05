f596e5e908a119257bb64bc0fc9900eb5627ce92
Murat Umutlu <murat@muum.dev>
2023-01-05 13:45:41 +0300

    changing structure

---
+++ src/components/atoms/buttons/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+
+interface ButtonProps {
+    width?: "w-full" | "w-1/2";
+
+}
+
+const Button: FC<ButtonProps> = ({width}) => {
+    return (
+        <button type="submit" className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Add to bag</button>
+    )
+}
+
+export default Button;

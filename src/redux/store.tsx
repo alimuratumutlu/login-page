b6bc46bace9dabb7ab76067f5e265a5c140d3bce
Murat Umutlu <murat@muum.dev>
2023-01-05 18:04:50 +0300

    typescript bugs fixed

---
+++ src/redux/store.tsx
@@ -0,0 +1,12 @@
+import userReducer from "./slices/userSlice";
+import { createStore, applyMiddleware } from "redux";
+import {
+	createStateSyncMiddleware,
+	initStateWithPrevTab,
+} from "redux-state-sync";
+
+const config = {};
+const middlewares = [createStateSyncMiddleware(config)];
+
+export const store = createStore(userReducer, applyMiddleware(...middlewares));
+initStateWithPrevTab(store);
